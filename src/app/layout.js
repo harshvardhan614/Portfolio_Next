fcbf0cca3071ba0b6ad5f15dd11a2528d1945c97
Harshvardhan Rathore <rharshvardhan614@gmail.com>
2024-03-12 00:08:01 +0500

    first commit in repo

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] , variable:"--font-inter" });
+
+export const metadata = {
+  title: "Harshvardhan's Portfolio",
+  description: "Developerd using next.js",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
