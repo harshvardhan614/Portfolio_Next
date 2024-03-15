be48ea8894dfc6e4e0631b98d4ffb1673074d66a
Harshvardhan Rathore <rharshvardhan614@gmail.com>
2024-03-16 01:32:24 +0500

    complete contact page

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import bg from "../../../../public/background/contact-background.png";
-import Form from "@/components/contact/Form";
+
+import ContactForm from "@/components/contact/ContactForm";
+import ContactDetail from "@/components/contact/ContactDetail";
 
 export const metadata = {
   title: "Contact",
@@ -17,21 +19,17 @@ export default function Contact() {
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
       />
 
-      <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
+      <article className="relative w-full flex flex-col items-center justify-center mt-0 sm:mt-10 py-8 sm:py-0 space-y-10">
         <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
           <h1 className="text-accent font-semibold text-center text-4xl capitalize">
-            summon the wizard
+            Let's Connect!
           </h1>
           <p className="text-center font-light text-sm xs:text-base">
-            Step into the circle of enchantment and weave your words into the
-            fabric of the cosmos. Whether you seek to conjure collaborations,
-            unlock mysteries, or simply share tales of adventure, your messages
-            are treasured scrolls within this realm. Use the form below to send
-            your missives through the ethereal network, and await the whisper of
-            magic in response.
+          Have a project in mind or just want to say hello? Feel free to reach out to me using the contact form below. I am always excited to hear about new opportunities and collaborations.Fill in the form with your details and message, and I will get back to you as soon as possible. Let's create something amazing together!
           </p>
         </div>
-        <Form />
+        <ContactDetail/>
+        <ContactForm/>
       </article>
     </>
   );
