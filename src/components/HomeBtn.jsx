f6d224855c28ba1b721ebe3e5a488c7cfbc6c49a
Harshvardhan Rathore <rharshvardhan614@gmail.com>
2024-03-18 18:46:37 +0500

    add cube assets sound change, homebtn update

--- src/components/HomeBtn.jsx
+++ src/components/HomeBtn.jsx
@@ -19,7 +19,7 @@ const HomeBtn = () => {
       name={"home"}
       prefetch={false}
     >
-      <span className="relative  w-14 h-14 p-4  hover:text-accent">
+      <span className="relative  w-10 h-10 xs:w-14 xs:h-14 p-2.5 xs:p-4   hover:text-accent">
         <Home className="w-full h-auto" strokeWidth={1.5} />
 
         <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
